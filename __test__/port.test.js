51e2e7e5658617d133ad9b8a80af52cf506a25a2
HJ Kang <hkang07@gmail.com>
2022-10-14 22:19:18 +0100

    DRYing port test done

--- __test__/port.test.js
+++ __test__/port.test.js
@@ -1,44 +1,39 @@
 const Port = require('../src/port');
 
-describe('Port constructor', () => {
-    it('instantiate a port object', () => {
-        expect(new Port('Dover')).toBeInstanceOf(Object);
+describe('Port', () => {
+    let artemis;
+    let voyage;
+    let dover;
+
+    beforeEach(() => {
+        artemis = {};
+        voyage = {};
+        dover = new Port('Dover');
+    })
+    describe('with ships and ports', () => {
+        it('instantiate a port object', () => {
+            expect(dover).toBeInstanceOf(Object);
+        });
+    
+        it('sets the name property', () => {
+            expect(dover.name).toEqual('Dover');
+        });
+
+        it('sets the ships property', () => {
+            expect(dover.ships).toEqual([]);
+        });
+
+        it('add ship to port', () => {    
+            dover.addShip(artemis);
+            expect(dover.ships).toContain(artemis);
+        });
+
+        it('remove ship from port', () => {
+            dover.addShip(artemis);
+            dover.addShip(voyage);
+            dover.removeShip(voyage);
+    
+            expect(dover.ships).not.toContain(voyage);
+        });
     });
-
-    it('sets the name property', () => {
-        const port = new Port('Dover');
-        expect(port.name).toEqual('Dover');
-    });
-});
-
-describe('Add ship', () => {
-    it('sets the ships property', () => {
-        const dover = new Port('Dover');
-        expect(dover.ships).toEqual([]);
-    });
-
-    it('add ship to port', () => {
-        const dover = new Port('Dover');
-        const artemis = {};
-
-        dover.addShip(artemis);
-        expect(dover.ships).toContain(artemis);
-    });
-});
-
-describe('Remove ship', () => {
-    it('remove ship from port', () => {
-        const artemis = {};
-        const voyage = {};
-        const magestic = {};
-        const dover = new Port('Dover');
-
-        dover.addShip(artemis);
-        dover.addShip(voyage);
-        dover.addShip(magestic);
-        dover.removeShip(voyage);
-
-        expect(dover.ships).not.toContain(voyage);
-    });
-
 });
